58a7ffc91eb10b17a171edeebaa2676db12da478
Doug Barth <dougbarth@gmail.com>
2008-12-26 10:59:51 -0600

    peek command uses base command class.

--- lib/amqp-utils/command.rb
+++ lib/amqp-utils/command.rb
@@ -6,5 +6,40 @@ require 'trollop'
 gem 'amqp'
 require 'mq'
 
+gem 'facets'
+require 'facets/string/tab'
+
 class Command
+  class << self
+    def run(args = ARGV)
+      command = new(args)
+      command.process_options
+      command.go
+    end
+  end
+
+  def initialize(args)
+    @args = args
+  end
+
+  attr_reader :args, :options
+
+  def process_options
+    command = self
+    @options = Trollop::options(@args) do
+      command.prepare_options(self) if command.respond_to?(:prepare_options)
+
+      opt :verbose, 'Print all AMQP commands sent and received.'
+    end
+  end
+
+  def go
+    EM.run do
+      AMQP.logging = options.verbose
+
+      trap("INT") { AMQP.stop { EM.stop } }
+
+      execute
+    end
+  end
 end
