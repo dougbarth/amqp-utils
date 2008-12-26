ec960995e91b6631220604f664a4075348c584fa
Doug Barth <dougbarth@gmail.com>
2008-12-26 11:49:05 -0600

    Connection configuration options.

--- lib/amqp-utils/command.rb
+++ lib/amqp-utils/command.rb
@@ -33,12 +33,22 @@ class Command
       |
       |Standard options:
       }.margin
+      opt :host, 'The AMQP host to connect to', :default => 'localhost'
+      opt :port, 'The AMQP port to connect to', :default => 5672
+      opt :vhost, 'The vhost to connect to', :default => '/'
+      opt :user, 'The user name to authenticate with', :default => 'guest'
+      opt :password, 'The password to connect with', :default => 'guest'
+      opt :timeout, 'The connect timeout in seconds', :default => 5
       opt :verbose, 'Print all AMQP commands sent and received.'
     end
   end
 
   def go
     EM.run do
+      %w(host port vhost user timeout).each do |val|
+        AMQP.settings[val.to_sym] = options[val.to_sym]
+      end
+      AMQP.settings[:pass] = options.password
       AMQP.logging = options.verbose
 
       trap("INT") { AMQP.stop { EM.stop } }
